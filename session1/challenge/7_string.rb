ee66549fcbd57e8fc063e61ac200c6a881e25ce5
Jolanta Jas <jolantajas@Jolantas-MacBook-Air.local>
2017-10-06 19:13:16 +0100

    session 1 ex7 7_string - done but code doesn't work

--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -1,8 +1,17 @@
 # given a string, return the character after every letter "r"
-# 
+#
 # pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  to_retun = ""
+    string.size.times do |n|
+      if n == "r" || n == "R"
+        return "#{string[n+1]}"
+      else
+        return ""
+      end
+    end
+    to_return
 end
