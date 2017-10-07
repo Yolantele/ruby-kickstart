9f29e3ef5c0db1f1e8e76b47fc4c3833628e9b35
Jolanta Jas <jolantajas@Jolantas-MacBook-Air.local>
2017-10-07 17:34:43 +0100

    Session 2 , Ex 1, 2 , 3 done , but with help of solutions

--- session2/challenge/3_array.rb
+++ session2/challenge/3_array.rb
@@ -1,3 +1,4 @@
+#TASK:
 # Write a method named every_other_char for strings that,
 # returns an array containing every other character
 #
@@ -5,7 +6,31 @@
 # "abcdefg".every_other_char  # => "aceg"
 # "".every_other_char         # => ""
 
+##initial code version
+#class String
+#  def every_other_char
+#  input = gets.chomp.to_s
+#  input.size.times.map {|n| n % 2 == 1}
+#  end
+#end
+
+## After tests using rake 2:3
+#class String
+#  def every_other_char
+#  input = gets.chomp.to_s
+#  to_return = ""
+#   input.each.map {|n| n % 2 == 1}
+#  end
+#  puts "#{String.every_other_char}"
+#end
+
+##After glimpsing the solution sheet
 class String
   def every_other_char
+    to_return = ''
+    each_char.with_index do |char, index|
+      to_return << char if index.even?
+    end
+    to_return
   end
 end
