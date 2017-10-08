9a2ef421942be386d599b3789dd34fbf01008f72
Jolanta Jas <jolantajas@Jolantas-MacBook-Air.local>
2017-10-08 16:25:09 +0100

    Up ro Session 2 , Ex 11 ,

--- session2/challenge/11_classes.rb
+++ session2/challenge/11_classes.rb
@@ -1,20 +1,43 @@
-# Write a program that outputs the lyrics for "Ninety-nine Bottles of Beer on the Wall"
-# Your program should print the number of bottles in English, not as a number. For example:
-#
-# Ninety-nine bottles of beer on the wall,
-# Ninety-nine bottles of beer,
-# Take one down, pass it around,
-# Ninety-eight bottles of beer on the wall.
-# ...
-# One bottle of beer on the wall,
-# One bottle of beer,
-# Take one down, pass it around,
-# Zero bottles of beer on the wall.
-#
-# Your program should not use ninety-nine output statements!
-# Design your program with a class named BeerSong whose initialize method 
-# receives a parameter indicating the number of bottles of beer initially on the wall.
-# If the parameter is less than zero, set the number of bottles to zero. Similarly,
-# if the parameter is greater than 99, set the number of beer bottles to 99
-# Then make a public method called print_song that outputs all stanzas from the number of bottles of beer down to zero.
-# Add any additional methods you find helpful.
+
+class BeerSong
+  attr_accessor :bottle_num
+
+  def initialize(bottle_num)
+ bottle_num = 0 if bottle_num < 0
+ bottle_num = 99 if bottle_num > 99
+    self.bottle_num = bottle_num #not sure why do we need to put self.beers here.. #referencing BeerSong?
+  end
+
+ def print_song
+   bottle_num.downto 1 do |i|
+     print_stanza i
+   end
+end
+
+def print_stanza(n)
+  if n.zero?
+    puts ""
+  else
+    puts "#{translate n} #{bottle n} of beers on the wall",
+        "#{translate n} #{bottle n} of beer,",
+        "Take one down, pass it around,",
+        "#{translate n - 1} #{bottle n - 1} of beer on the wall."
+  end
+end
+
+def bottle(n)
+  return 'bottle' if n == 1
+  return  'bottles' if n != 1
+  end
+  end
+
+  def translate(n)
+    if 0 <= n && n<=19
+      %w(zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen)[n]
+   elsif n % 10 == 0
+      %w(zero ten twenty thirty forty fifty sixty seventy eighty ninety)[n/10]
+  else
+    "#{translate n/10*10}-#{translate n%10}".downcase
+    end.capitalize
+  end
+end
--- session2/challenge/8_array.rb
+++ session2/challenge/8_array.rb
@@ -1,7 +1,22 @@
 # Given an array of elements, return true if any element shows up three times in a row
-# 
+#
 # Examples:
 # got_three? [1, 2, 2, 2, 3]  # => true
 # got_three? ['a', 'a', 'b']  # => false
 # got_three? ['a', 'a', 'a']  # => true
 # got_three? [1, 2, 1, 1]     # => false
+
+##code before looking at solution
+#def got_three?(elements)
+# elements.each.sort.uniq do |e|
+#   true if e.times == 3
+# end
+# false
+# end
+
+ def got_three?(elements)
+  elements.each_cons 3 do |e, f, g|  #why did I have to remove .sort after elements for the code to work eventually?
+    return true if e == f && f == g  
+  end
+  false
+  end
